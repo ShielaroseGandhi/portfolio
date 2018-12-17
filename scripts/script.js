6b75a4a5afee1b3e17e83ab14a0f63526924c68a
Shielarose <shielarosegandhi@gmail.com>
2018-12-16 21:51:43 -0500

    started on portfolio section

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,37 @@
 // Doc Ready
-let typed = new Typed(".typed", {
-   strings: ["I'm a front-end web developer"],
-   stringsElement: null,
-   startDelay: 1500,
-   typeSpeed: 50,
-   backSpeed: 50,
-   smartBackspace: true,
-   backDelay: 1200,
-   loop: false,
-   loopCount: Infinity,
-   showCursor: false,
-   cursorChar: '|',
-   autoInsertCss: true,
-   callback: function() {}
-});
 $(function(){
    // uncheck checkbox when link in nav is clicked
    $(".main-nav__menu--link").on("click", function(){
       $("input[type=checkbox]").prop("checked", false)
    })
+   
+   // typed js
+   let typed = new Typed(".typed", {
+      strings: ["I'm a front-end web developer"],
+      stringsElement: null,
+      startDelay: 1500,
+      typeSpeed: 50,
+      backSpeed: 50,
+      smartBackspace: true,
+      backDelay: 1200,
+      loop: false,
+      loopCount: Infinity,
+      showCursor: false,
+      cursorChar: '|',
+      autoInsertCss: true,
+      callback: function() {}
+   });
 
+   // play video on hover of the image 
+   $(".portfolio__screenshot").hover(function(){
+      $(this).addClass("hidden"),
+      $("video").removeClass("hidden")
+   })
+
+   // once video has ended, add portrait img on top again
+   $('video').on('ended', function () {
+      $(this).addClass("hidden"),
+      $(".portfolio__screenshot").removeClass("hidden")
+   });
 
 });
