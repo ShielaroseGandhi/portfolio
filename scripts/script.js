9ba1b768a543a7dc30c071be6582d5f4d1a5bf35
Shielarose <shielarosegandhi@gmail.com>
2019-01-07 15:39:14 -0500

    added form validation

--- scripts/script.js
+++ scripts/script.js
@@ -52,4 +52,58 @@ $(function(){
          window.scrollTo(0, window.pageYOffset);
    });
 
+   // form validation
+   formValidation = () => {
+      $(".contact__input").on("keydown", function(e){
+         if ($("#name").is(":focus")) {
+            $(".error").remove()
+            $("#name").removeClass("contact__input--error")
+         } else if ($("#message").is(":focus")) {
+            $(".error").remove()
+            $("#message").removeClass("contact__input--error")
+         } else if ($("#email").is(":focus")) {
+            $(".error").remove()
+            $("#email").removeClass("contact__input--error")
+         }
+      })
+
+      $(".contact__form").submit(e => {
+         let name = $("#name").val();
+         let email = $("#email").val();
+         let message = $("#message").val();
+
+         let regexName = /^[a-z ,.'-]+$/i
+         let validName = regexName.test(name)
+
+         let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+         let validEmail = regexEmail.test(email);
+
+         $(".error").remove()
+         if (name.length < 1) {
+            e.preventDefault();
+            $("#name").after(`<span class="error">Please enter your name</span>`)
+            $("#name").addClass("contact__input--error")
+         } else if (!validName){
+            e.preventDefault();
+            $("#name").after(`<span class="error">Sorry, numbers and special characters are invalid</span>`)
+            $("#name").addClass("contact__input--error")
+         }
+         if (message.length < 10) {
+            e.preventDefault();
+            $("#message").after(`<span class="error">Your message needs to be at least 10 characters long.</span>`)
+            $("#message").addClass("contact__input--error")
+         }
+         if (email.length < 1) {
+            e.preventDefault();
+            $("#email").after(`<span class="error">This field is required</span>`)
+            $("#email").addClass("contact__input--error")
+         } else if (!validEmail) {
+            e.preventDefault();
+            $("#email").after(`<span class="error">Enter a valid email</span>`);
+            $("#email").addClass("contact__input--error")
+         }
+      })
+   }
+
+   formValidation()
 });
